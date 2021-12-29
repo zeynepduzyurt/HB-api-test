a32484c132647430164eac1bc876f508d7ad20e0
zeynep.duzyurt <zeynepduzyurt34@gmail.com>
2021-12-29 22:43:05 +0300

    .gitignore file added

---
+++ src/main/java/User.java
@@ -0,0 +1,12 @@
+public class User {
+    public int id = 5;
+    public String username = "2ez4zeynep";
+    public String firstName="Zeynep";
+    public String lastName="Duzyurt";
+    public String email="zeynepduzyurt34@gmail.com";
+    public String password="123.";
+    public String phone="531846456";
+    public long userStatus=10;
+
+
+}
